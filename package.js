400a1a147f6888aac6db3a0300280c2873c55737
Brad <th.lee@ecubelabs.com>
2015-06-18 10:47:36 +0900

    version changed

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'taehee:image-saver',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'Save images to server from url',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/kairess/meteor-image-saver',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
